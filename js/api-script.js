a3218e0a03370393141867596c719854d1d30dda
Angel Bencosme <angelbencosme2022@gmail.com>
2026-02-11 12:53:10 -0500

    added changes

--- js/api-script.js
+++ js/api-script.js
@@ -31,7 +31,11 @@ document.getElementById("searchSubmit").addEventListener("click", () => {
            - Only use the Storage API to save the API key if one was inputted
 */
 function saveKey() {
-  
+  const key = document.getElementById("key").value.trim();
+  if (key) {
+    localStorage.setItem("unsplashAPIKey", key);
+  }
+
 }
 
 
@@ -42,6 +46,14 @@ function saveKey() {
            - Update the visibiltity to show the photo gallery search setup (hint: look at the HTML to access and CSS property)
 */
 function hideKeySetup() {
+  const keySetup = document.getElementById("keySetup");
+  keySetup.remove();
+
+
+  const gallerySetup = document.getElementById("gallerySetup");
+  gallerySetup.style.visibility = "visible";
+
+
   
 }
 
@@ -69,7 +81,29 @@ function hideKeySetup() {
              - Output error to console
              - Update photo gallery message to "Something went wrong..." 
 */
-function getImages(term) {
+async function getImages(term) {
+  
+  const gallery = document.getElementById("gallery");
+  const apiKey = localStorage.getItem("unsplashAPIKey");
+
+  gallery.innerHTML="";
+
+  gallery.innerHTML = "<p>Loading...:</p>";
+
+  const url = `https://api.unsplash.com/search/photos?query=${encodeURIComponent(term)}&page=1&per_page=15&client_id=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    
+    displayImages(data, term);
+  } catch (error) {
+    console.error("Error fetching images:", error);
+    gallery.innerHTML = "<p>Something went wrong...</p>";
+
+
+  }
   
 }
 
@@ -90,4 +124,27 @@ function getImages(term) {
 */
 function displayImages(data, term) {
   
+  const gallery = document.getElementById("gallery");
+  
+  gallery.innerHTML = "";
+
+  const results = data.results;
+
+  if (results.length === 0) {
+    gallery.innerHTML = `<p>No result returned for ${term}.</p>`;
+  
+    return;
+  }
+  
+  for (let i =0; i < results.length && i < 15; i++){
+    const img = document.createElement("img");
+    img.src = results[i].urls.small;
+    img.alt = results[i].alt_description ? results[i].alt_description : term;
+    img.title = results[i].user.name ? results[i].user.name : "";
+    gallery.appendChild(img);
+  }
+    
+  
 }
+
+
